b24ed3c53399e7de5b25843640cb0ccb08368a58
Michael Matuzak <mmatuzak@gmail.com>
2021-12-11 14:15:57 -0800

    more tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,32 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use tuzos::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop{}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     tuzos::test_panic_handler(info)
 }
 
+use tuzos::println;
+
 #[test_case]
-fn simple_test() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
