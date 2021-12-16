6e07db7b3f129f7bfea94e2514419efef97de660
Michael Matuzak <mmatuzak@gmail.com>
2021-12-15 20:03:18 -0800

    memory stuff

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,39 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 use tuzos::println;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use tuzos::memory;
+    use tuzos::memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Booting TuzOS {}", VERSION);
     tuzos::init();
 
-    // fn stack_overflow() {
-    //     stack_overflow(); // for each recursion, the return address is pushed
-    // }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    // trigger a stack overflow
-    // stack_overflow();
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    // trigger a page fault
-    // unsafe {
-    //     *(0xdeadbeef as *mut u64) = 42;
-    // };
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     tuzos::hlt_loop();
 }
 
